5495d00a67b1e84dc6ef9e41382bbb35dc81b988
t-drape <thebatreview1@gmail.com>
2024-07-29 16:41:48 -0700

    Solve all problems, create one new class length problem

---
+++ lib/gretting.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+def salutation(player = nil)
+  print_out
+  if player
+    puts "#{current_player} is the winner!"
+  else
+    puts "Nobody won! It's a tie!"
+  end
+end
+
+def greet
+  puts 'Use Format column, row '
+  puts 'Please select an available move: '
+end
+
+def print_out
+  p board[0]
+  p board[1]
+  p board[2]
+end
