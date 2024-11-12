574ab9b96f6210894ab3743f968e13776dcc6974
t-drape <thebatreview1@gmail.com>
2024-11-11 19:14:40 -0700

    Add user-determined names in main.rb

--- main.rb
+++ main.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 require_relative 'lib/tic_tac_toe'
-require_relative 'lib/play'
 
-player_one, player_two = users
-
-new_game = Game.new(player_one, player_two)
+puts 'Player One: '
+p_one = gets.chomp
+puts 'Player Two: '
+p_two = gets.chomp
+new_game = Game.new(p_one, p_two)
 
 new_game.play_game
